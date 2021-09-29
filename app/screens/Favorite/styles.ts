72d963a9fc716cf53bb49b9b96216d656d6fce1a
Gsoft02 <gsoft02@Gsoft02s-MacBook-Pro.local>
2021-09-29 17:14:24 +0500

    Explore done

--- app/screens/Favorite/styles.ts
+++ app/screens/Favorite/styles.ts
@@ -1,12 +1,11 @@
 import { StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
-import {
-    heightPercentageToDP as hp,
-    widthPercentageToDP as wp,
-} from 'react-native-responsive-screen';
+import { getPercentageHeight, getPercentageWidth } from 'utils/dimentionUtil';
 import { Dimensions } from 'react-native';
 export const useStyles = () => {
     const theme = useTheme();
+    const width = getPercentageWidth();
+    const height = getPercentageHeight();
     const windowWidth = Dimensions.get('window').width;
     const windowHeight = Dimensions.get('window').height;
     const styles = StyleSheet.create({
@@ -19,7 +18,7 @@ export const useStyles = () => {
             paddingBottom: 2,
         },
         mainShimmerView: {
-            height: hp('100%'),
+            height: height('100%'),
             justifyContent: 'center',
             alignItems: 'center',
         },
@@ -27,8 +26,8 @@ export const useStyles = () => {
             margin: 2,
         },
         imageError: {
-            height: hp('50%'),
-            width: wp('50%'),
+            height: height('50%'),
+            width: width('50%'),
             tintColor: theme.colors.text,
         },
         containerView: {
@@ -38,19 +37,19 @@ export const useStyles = () => {
             alignItems: 'stretch',
         },
         name: {
-            fontSize: hp('3%'),
+            fontSize: height('3%'),
             fontWeight: 'bold',
             fontFamily: 'Avenir-Medium',
             alignItems: 'center',
         },
         tagLine: {
-            fontSize: hp('2%'),
+            fontSize: height('2%'),
             fontFamily: 'Avenir-Medium',
             alignItems: 'center',
         },
         listCaption: {
             marginTop: 15,
-            fontSize: hp('2%'),
+            fontSize: height('2%'),
             fontFamily: 'Avenir-Medium',
             alignItems: 'center',
             fontWeight: 'bold',
@@ -67,7 +66,7 @@ export const useStyles = () => {
             marginTop: 30,
         },
         flatList: {
-            height: windowHeight > windowWidth ? hp('100%') : wp('100'),
+            height: windowHeight > windowWidth ? height('100%') : width('100'),
             marginTop: 10,
             flexDirection: 'column',
             marginBottom: 30,
@@ -77,20 +76,20 @@ export const useStyles = () => {
             backgroundColor: theme.colors.background,
         },
         books: {
-            height: hp('2%'),
-            width: wp('1%'),
+            height: height('2%'),
+            width: width('1%'),
             borderRadius: 4,
         },
 
         bookTrending: {
-            height: hp('25%'),
-            width: wp('37%'),
+            height: height('25%'),
+            width: width('37%'),
             borderRadius: 4,
         },
 
         bookGeneral: {
-            height: hp('20%'),
-            width: wp('30%'),
+            height: height('20%'),
+            width: width('30%'),
             borderRadius: 4,
         },
         bookView: {
