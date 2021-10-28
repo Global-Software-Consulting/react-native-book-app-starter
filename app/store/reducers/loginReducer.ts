7fc6ffb9d3b57fcd564ab928b7bb4d82743160ec
hannadahmad <hannadahmad@gmail.com>
2021-10-28 14:33:52 +0500

    Reducer tests

--- app/store/reducers/loginReducer.ts
+++ app/store/reducers/loginReducer.ts
@@ -22,41 +22,13 @@ export const loginReducer = createReducer(initialState, {
     [types.LOGIN_LOADING_ENDED](state: ILoginState) {
         return { ...state };
     },
-    [types.LOGIN_RESPONSE](state: ILoginState, action: ILoginResponseState) {
-        return {
-            ...state,
-            loginResponse: action.payload,
-        };
-    },
-    [types.LOGIN_FAILED](state: ILoginState) {
-        return {
-            ...state,
-            isLoggedIn: false,
-        };
-    },
-    [types.SET_TOKEN](state: ILoginState, action: ILoginRequestState) {
-        return {
-            ...state,
-            token: action.payload,
-        };
-    },
-    [types.LOG_OUT](state: ILoginState) {
-        return {
-            ...state,
-            isLoggedIn: false,
-        };
-    },
     [types.SET_LOGGED_IN](state: ILoginState) {
         return {
             ...state,
             isLoggedIn: true,
         };
     },
-    [types.USER_DETAILS_REQUEST](state: ILoginState) {
-        return {
-            ...state,
-        };
-    },
+    
     [types.USER_DETAILS_RESPONSE](state: ILoginState, action: ILoginResponseState) {
         return {
             ...state,
