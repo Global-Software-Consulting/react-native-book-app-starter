1862167fcefcf83e613623a1d70121dfc52a1116
hannadahmad <hannadahmad@gmail.com>
2021-11-12 09:44:13 +0500

    minor bugs fixed

--- app/screens/BookReader/index.tsx
+++ app/screens/BookReader/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import { View } from 'react-native'
-import Pdf from 'react-native-pdf'
+import { Platform, View } from 'react-native'
+// import Pdf from 'react-native-pdf';
+import PDFView from 'react-native-view-pdf/lib/index'
+
 import { useStyles } from './styles'
+const resources = {
+    file:
+        Platform.OS === 'ios'
+            ? 'downloadedDocument.pdf'
+            : '/sdcard/Download/downloadedDocument.pdf',
+    url: 'https://www.adelaide.edu.au/writingcentre/sites/default/files/docs/learningguide-articlesinenglishgrammar.pdf',
+    base64: 'JVBERi0xLjMKJcfs...',
+}
 const BookReader: React.FC = () => {
     const src = {
         uri: 'https://www.adelaide.edu.au/writingcentre/sites/default/files/docs/learningguide-articlesinenglishgrammar.pdf',
     }
     const styles = useStyles()
+    const resourceType = 'url'
 
     return (
-        <View style={styles.mainView}>
-            <Pdf source={src} style={styles.pdfView} />
+        <View>
+            <PDFView
+                fadeInDuration={250.0}
+                style={{ width: '100%', height: '100%' }}
+                resource={resources[resourceType]}
+                resourceType={resourceType}
+            />
         </View>
     )
 }
